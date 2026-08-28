a1721e9ff24ac8872d83085f2f4c6e8a6ef1fde4
Christophe Branchereau <cbranchereau@proton.me>
2026-08-28 08:40:25 +0200

    celeri

--- files/scripts/create-user.sh
+++ files/scripts/create-user.sh
@@ -15,7 +15,6 @@ done
 
 install -d -m 0700 -o christophe -g christophe /var/home/christophe
 chmod 0700 /var/home/christophe
-cp -af /etc/skel/. /var/home/christophe/
 chown -R christophe:christophe /var/home/christophe
 
 echo 'christophe:christophe' | chpasswd
